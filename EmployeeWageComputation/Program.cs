aaeb5f3a80322834fa47e9e252f2e4116206d2a8
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-15 19:15:39 +0530

    [Hemalatha] Refactor:Implimentation of Employee Wage program till a condition of total working hours or days is reached for a month

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,12 +8,14 @@ namespace EmployeeWageComputation
         public const int IS_PART_TIME = 1;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to employee wage computation program");
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -28,10 +30,10 @@ namespace EmployeeWageComputation
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days: " + totalWorkingDays + "Emp Hrs: " + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage: " + totalEmpWage);
         }
     }
