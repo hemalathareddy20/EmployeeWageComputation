80050e4e2233f9f1e9eff1d24289627e0fe150f3
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-15 12:11:33 +0530

    [Hemalatha] Add:Implimentation of employee Attendance program

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
