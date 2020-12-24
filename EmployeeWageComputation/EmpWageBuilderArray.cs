3f878bb8eb2fa74e02f7d46f926ed40bbe2c9721
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-24 11:30:38 +0530

    [Hemalatha] Refactor:Implementation to get Employee Total Wage When Queried by Company

--- EmployeeWageComputation/EmpWageBuilderArray.cs
+++ EmployeeWageComputation/EmpWageBuilderArray.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace EmployeeWageComputation
 {
-    public class EmpWageBuilderArray:IComputeEmpWage
+    public class EmpWageBuilderArray : IComputeEmpWage
     {
         public const int IS_FULL_TIME = 2;
         public const int IS_PART_TIME = 1;
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,6 +11,7 @@ namespace EmployeeWageComputation
             empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
             empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
             empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total Wage for DMart company:" + empWageBuilder.getTotalWage("DMart"));
         }
     }
 }
